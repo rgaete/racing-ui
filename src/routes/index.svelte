5e5368f6776299935d3d90b9de71d089bbd4073f
Ricardo Gaete <ricardo.gaete@walmart.com>
2020-05-21 00:07:38 -0400

    first commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,21 @@
+<script>
+  import News from "../components/News.svelte";
+  import Footer from "../components/Footer.svelte";
+  import Header from "../components/Header.svelte";
+  import HeroSlider from "../components/HeroSlider.svelte";
+  import PushyPanel from "../components/PushyPanel.svelte";
+  import ModalLogin from "../components/ModalLogin.svelte";
+</script>
+
+<div class="site-wrapper clearfix">
+  <div class="site-overlay" />
+  <div id="root">
+    <div id="home">
+      <Header />
+      <PushyPanel />
+      <HeroSlider />
+      <News />
+      <Footer />
+    </div>
+  </div>
+</div>
